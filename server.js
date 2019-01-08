43150c0156b71f87d657ebed6f20b3260e9b46a7
Tanushree Chakravarty <tanushree.vawsum@gmail.com>
2019-01-08 22:56:21 +0530

    Registered Users

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const users = require("./routes/api/users");
 
